eb8f76c9534266c09e8f0fcde65532443ecf8640
Ilya Rubinshtein <rubinshtein.ilya@gmail.com>
2025-12-25 18:08:32 +0500

    added products

--- src/assets/js/product-management.js
+++ src/assets/js/product-management.js
@@ -1,3 +1,151 @@
+const products = [
+  {
+    warehouse: "PP1",
+    article: "123456789",
+    code: "123456789",
+    title: "Детский Велик",
+    price: "10 000 ₸",
+    remainder: "34",
+    preorder: "34",
+    dumping: "Не настроен",
+    status: "warning",
+    stock: "34",
+    min: "34",
+    total: "34",
+    avr_sales: "34",
+    days_since_last_sale: "34",
+    last_sale: "16.10.2025",
+    last_price: "34",
+    parent: "34",
+    present: "34",
+  },
+  {
+    warehouse: "PP1",
+    article: "123456789",
+    code: "123456789",
+    title: "Детский Велик",
+    price: "10 000 ₸",
+    remainder: "34",
+    preorder: "34",
+    dumping: "Не настроен",
+    status: "preorder",
+    stock: "34",
+    min: "34",
+    total: "34",
+    avr_sales: "34",
+    days_since_last_sale: "34",
+    last_sale: "16.10.2025",
+    last_price: "34",
+    parent: "34",
+    present: "34",
+  },
+  {
+    warehouse: "PP1",
+    article: "123456789",
+    code: "123456789",
+    title: "Детский Велик",
+    price: "10 000 ₸",
+    remainder: "34",
+    preorder: "34",
+    dumping: "Не настроен",
+    status: "success",
+    stock: "34",
+    min: "34",
+    total: "34",
+    avr_sales: "34",
+    days_since_last_sale: "34",
+    last_sale: "16.10.2025",
+    last_price: "34",
+    parent: "34",
+    present: "34",
+  },
+  {
+    warehouse: "PP1",
+    article: "123456789",
+    code: "123456789",
+    title: "Детский Велик",
+    price: "10 000 ₸",
+    remainder: "34",
+    preorder: "34",
+    dumping: "Не настроен",
+    status: "success",
+    stock: "34",
+    min: "34",
+    total: "34",
+    avr_sales: "34",
+    days_since_last_sale: "34",
+    last_sale: "16.10.2025",
+    last_price: "34",
+    parent: "34",
+    present: "34",
+  },
+];
+
+function createDesktopProduct(product) {
+  return `
+  <tr>
+  <th scope="row">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="card" />
+      <label class="form-check-label" for="card"></label>
+    </div>
+  </th>
+  <td>
+    <img src="assets/images/profibot/bike.png" alt="product image" />
+  </td>
+  <td>${product.warehouse}</td>
+  <td class="text-primary fw-bold">${product.article}</td>
+  <td class="text-primary fw-bold">${product.code}</td>
+  <td>
+    <input type="text" class="form-control table-input-profi" placeholder="" value="${product.title}" />
+  </td>
+  <td class="fw-bold">${product.price}</td>
+  <td>${product.remainder}</td>
+  <td>${product.preorder}</td>
+  <td>
+    <div class="d-flex align-items-center gap-1">
+      <div class="td-profi-text">Не настроен</div>
+      <img src="assets/images/profibot/edit.svg" alt="Edit icon" style="cursor: pointer" />
+    </div>
+  </td>
+  <td class="text-start">
+    <div class="d-flex align-items-center gap-1">
+      <span class="badge-profi badge-profi-${product.status}">
+      ${product.status === "success" ? "В наличии" : product.status === "warning" ? "Сняты с продажи" : "Предзаказ"}
+      </span>
+
+      <div class="dropdown">
+        <button
+          class="badge-btn-profi cursor-pointer dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton7"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          ⋮
+        </button>
+        <ul class="dropdown-menu h-fit" aria-labelledby="dropdownMenuButton7">
+          <li><a class="dropdown-item" href="#">Списать остатки</a></li>
+          <li><a class="dropdown-item" href="#">Настроить предзаказ</a></li>
+          <li><a class="dropdown-item" href="#">Снять с продажи</a></li>
+          <li><a class="dropdown-item" href="#">Настроить мин. остатки</a></li>
+        </ul>
+      </div>
+    </div>
+  </td>
+  <td>${product.stock}</td>
+  <td>${product.min}</td>
+  <td>${product.total}</td>
+  <td>${product.avr_sales}</td>
+  <td>${product.days_since_last_sale}</td>
+  <td>${product.last_sale}</td>
+  <td>${product.last_price}</td>
+  <td>${product.parent}</td>
+  <td>${product.present}</td>
+</tr>
+  `;
+}
+
 $(document).ready(function () {
   // Открытие модального окна при клике на кнопку "Управление остатками"
   $("#leftovers").click(function () {
@@ -60,5 +208,11 @@ $(document).ready(function () {
   // Отключение/включение всех чекбоксов (кроме тех, что внутри form-switch)
   $("#mainCheckbox").click(function () {
     $(".form-check-input").not(".form-switch .form-check-input").prop("checked", this.checked);
+  });
+
+  // Добавляем карточки товаров
+  const $tpagination = $("#t-pagination");
+  products.forEach((product) => {
+    $tpagination.before(createDesktopProduct(product));
   });
 });
